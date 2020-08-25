85cb2fb4c4a36ecde2b88def35e37a5ba465b5e1
Tomoichi Ebata <ebata20171024@kobore.net>
2020-08-25 15:16:06 +0900

    second_trial

--- server.go
+++ server.go
@@ -17,7 +17,7 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var addr = flag.String("addr", "localhost:8080", "http service address")
+var addr = flag.String("addr", "localhost:8088", "http service address")
 
 var upgrader = websocket.Upgrader{} // use default options
 
